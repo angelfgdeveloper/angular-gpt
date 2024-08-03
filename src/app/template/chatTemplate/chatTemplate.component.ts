c671b68780de9af1e762b8f22bf2b2ae4dc4641c
Luis Angel <angelfgdeveloper@gmail.com>
2024-08-02 20:10:47 -0600

    Service y chatTemplate

--- src/app/template/chatTemplate/chatTemplate.component.ts
+++ src/app/template/chatTemplate/chatTemplate.component.ts
@@ -1,29 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, TextMessageBoxEvent, TextMessageBoxFileComponent, TextMessageBoxSelectComponent, TextMessageEvent, TypingLoaderComponent } from '@components/index';
 import { Message } from '@interfaces/message.interface';
-import { OpenAiService } from '../../services/openai.service';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
-  selector: 'app-orthography-page',
+  selector: 'app-chat-template',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
 
     ChatMessageComponent,
     MyMessageComponent,
     TypingLoaderComponent,
+
     TextMessageBoxComponent,
     TextMessageBoxFileComponent,
     TextMessageBoxSelectComponent,
   ],
-  templateUrl: './orthographyPage.component.html',
+  templateUrl: './chatTemplate.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent {
+export class ChatTemplateComponent {
 
-  public messages = signal<Message[]>([ {text: 'Hola Mundo', isGpt: false } ]);
+  public messages = signal<Message[]>([]);
   public isLoading = signal(false);
   public openAiService = inject(OpenAiService);
 
